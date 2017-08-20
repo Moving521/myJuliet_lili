9a87daf0e51cea6a37a33aff7b8a3baa5045513c
Moving521 <515504936@qq.com>
2017-08-20 21:44:25 +0800

    0000美女

--- src/test/demo/javaBasic/UserInterfaceImpl.java
+++ src/test/demo/javaBasic/UserInterfaceImpl.java
@@ -17,7 +17,7 @@ public class UserInterfaceImpl implements UserInterface {
     @Override
     public String see() {
 
-        return "看到一群||00后美女";
+        return "看到一群00后美女";
     }
 
     @Override
